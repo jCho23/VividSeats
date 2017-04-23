06d840090eb3d1377dd371e09b8ddcab25b97c71
JuneCho23 <junecho.ucb@gmail.com>
2017-04-23 12:02:09 -0700

    Screenshots

--- VividSeats/Tests.cs
+++ VividSeats/Tests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
+using System.Net.Configuration;
 
 namespace VividSeats
 {
@@ -32,12 +33,24 @@ namespace VividSeats
 		}
 
 		[Test]
-		public void FirstTest()
+		public void BrowseAndCheckoutTest()
 		{
 			app.Tap(x => x.Class("android.widget.ImageButton").Index(0));
-
+			app.Screenshot("Let's start by Tapping on the 'Hamburger' Button ");
 			app.Tap("Browse");
 			app.Screenshot("We Tapped the 'Browse' Button");
+
+			app.Tap("item_name");
+			app.Screenshot("Next we Tapped on the first result");
+			app.Tap("item_ticket_line1");
+			app.Screenshot("Then we Tapped on the 'Ticket'");
+
+			app.Tap("ticket_details_checkout_button");
+			app.Screenshot("We Tapped on the 'Checkout' Button");
+			app.Tap("2");
+			app.Screenshot("Next we Tapped on '2 Tickets'");
+			app.Tap("ticket_details_checkout_button");
+			app.Screenshot("Then we Tapped on the 'Checkout' Button again");
 		}
 	}
 }
